ee3b438bde0e9ff1cb4c228fd577ed986f9714e2
AMWOLLO VICTOR <amwollo@yahoo.com>
2019-03-27 14:53:32 +0300

    If else and when

---
+++ src/Ifelse.kt
@@ -0,0 +1,22 @@
+//control flow with if else
+
+fun main() {
+    println("Enter any digit: ")
+    val name = readLine()
+
+    if (name.equals("Victor") ) {
+        println("Yes its Victor ")
+    } else {
+        println("Its $name, we wanted Victor ")
+    }
+
+    //this could be null
+    val  person:String?
+    when(name) {
+        "VA" -> person = "Victor Amwollo"
+        "VI" -> person = "AMWOLLO VIC"
+        else ->person  = "Unknown"
+    }
+
+    println("========The person is  $person")
+}
