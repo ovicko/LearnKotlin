91044656e5f5b95cb85941ac55e37f40632959c5
AMWOLLO VICTOR <amwollo@yahoo.com>
2019-03-28 16:22:26 +0300

    Manage unordered collections

--- src/Arrays.kt
+++ src/Arrays.kt
@@ -7,4 +7,20 @@ fun main() {
     for (item:Int in intArray) {
         println(item)
     }
+
+    val colorList:List<String> = listOf("Green","Black","Red")
+    println(colorList)
+
+    val colorList2 :MutableList<String> = mutableListOf("Green","Black","Red")
+    colorList2.add("Yellow")
+    colorList2.add("Purple")
+    colorList2.add("Orange")
+    colorList2.add("Gray")
+
+    println(colorList2)
+    colorList2.sortDescending()
+    println(colorList2)
+
+
+
 }
