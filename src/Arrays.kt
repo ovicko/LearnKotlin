9c27606c1886b9f00651151db3d84836a9380664
AMWOLLO VICTOR <amwollo@yahoo.com>
2019-03-28 16:14:48 +0300

    Intro to arrays

---
+++ src/Arrays.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val intArray:IntArray = intArrayOf(23,3,14,52,16)
+    //sort array
+    intArray.sort()
+
+    //loop through
+    for (item:Int in intArray) {
+        println(item)
+    }
+}
