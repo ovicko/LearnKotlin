05d25605f1655d695cc8b65e1b9d90874f99c971
AMWOLLO VICTOR <amwollo@yahoo.com>
2019-03-28 10:25:09 +0300

    Declare and use data class

---
+++ src/oop/Clothing.kt
@@ -0,0 +1,6 @@
+package oop
+
+fun main() {
+    val item = ClothingItem("Shirt","L",15.89)
+    println(item)
+}
---
+++ src/oop/ClothingItem.kt
@@ -0,0 +1,5 @@
+package oop
+
+data class ClothingItem (val type:String,
+                         val size:String,
+                         val price:Double)
